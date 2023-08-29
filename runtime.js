cf96ff4adc851bc897be6b6bb6813ec284317ef4
Shawn Wagner <Shawnwagner2814@gmail.com>
2023-08-29 19:25:52 -0400

    updated test

--- runtime.js
+++ runtime.js
@@ -65,15 +65,51 @@ console.log("append", resultsAppend.preciseWords);
 
 
 //SUM ZERO//
-// function sumZero(array[i]) {
-//     for(let i = 0; i < array.length; i++){
-//             if(arr[i] === -arr[i]){
-//             console.log('true')
-//           }
-//      }
+
+const addToZero = (arr) => {
+
+    let defaultOutput = false
+
+    for(let i = 0; i < arr.length; i++) {
+        for(let j = i + 1; j < arr.length; j++) {
+
+            if(arr[i] + arr[j] === 0) {
+                defaultOutput = true;
+            }
+        }
+    }
+
+    return defaultOutput;
+
+};
+
+console.log(addToZero([1,2,3,-2]));
+
+// const arr0 = [3];
+// const arr1 = [4];
+// const arr2 = [0];
+// const arr3 = [2, 8, -9, 1];    
+
+// const arr = [arr0, arr1, arr2, arr3]
+
+// function zeroSum(arr) {
+//     const out = [];
+//     for(let i = 0; i < arr.length; i++) {
+//         let sum = 0;
+//         const inner = arr[i];
+//         for(let j = 0; j < inner.length; j++) {
+//             sum += inner[j];
+//         }
+//         if (sum === 0) {
+//             out.push(true);
+//         } else {
+//             out.push(false);
+//         }
+//     }
+//     return out;
 // }
-// sumZero(array[i]) // O(1)
-//not sure how to fully do this
+
+console.log(zeroSum(arr)); //O(n^2)
 
 //UNIQUE CHARACTERS//
 function uniqueChars(str) {
@@ -87,7 +123,7 @@ function uniqueChars(str) {
     } 
     return boolean
 } 
-console.log(uniqueChars('shawn'))
+console.log(uniqueChars('shawn')) //O(1)
 
 
 //PANGRAM SENTENCE//
@@ -109,14 +145,31 @@ const isPangram = (str = '') => {
 console.log(isPangram(str)) // O(n)
 
 //LONGEST WORD//
-function findLongestWord(str) {
-    let strSplit = str.split(' ');
-    let longestWord = 0;
-    for(let i = 0; i < strSplit.length; i++){
-        if(strSplit[i].length > longestWord) {
-            longestWord = strSplit[i].length;
-        }
+// function findLongestWord(str) {
+//     let strSplit = str.split(' ');
+//     let longestWord = 0;
+//     for(let i = 0; i < strSplit.length; i++){
+//         if(strSplit[i].length > longestWord) {
+//             longestWord = strSplit[i].length;
+//         }
+//     }
+//     return longestWord;
+// }
+// console.log(findLongestWord("the quick brown fox jumped over the lazy dog"));
+
+const findLongestWord = (arr) => {
+
+let longestDefault = arr[0].length;
+
+for(let i = 0; i < arr.length; i++) {
+    
+    if(arr[i].length > longestDefault) {
+        longestDefault = arr[i].length;
     }
-    return longestWord;
 }
-console.log(findLongestWord("the quick brown fox jumped over the lazy dog"));
+
+return longestDefault;
+
+};
+
+console.log(findLongestWord(['hi','hello', 'yoyoyoyoyo']));
